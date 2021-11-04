b1f4b982ccb98e43da79a0db98c2ebbfe19622d9
Ivan Nesterov <71551187+nesterrovv@users.noreply.github.com>
2021-11-04 20:44:07 +0300

    Removed simple explanatory comments for customer.

--- string-calculator/src/Main.java
+++ string-calculator/src/Main.java
@@ -5,22 +5,22 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        try { // если вдруг ввод будет совсем не корректный, то перейдет в блок catch и сообщит об этом
+        try { 
             String[] expression = new String[3];
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter your expression here:");
-            String enter = scanner.nextLine(); // Читаем с клавиатуры ввод
-            Pattern p = Pattern.compile("\"([^\"]*)\""); // компилируем регулярное выражение, которое будет выуживать информацию между кавычек
-            Matcher m = p.matcher(enter); // применение регулярки
+            String enter = scanner.nextLine(); 
+            Pattern p = Pattern.compile("\"([^\"]*)\"");
+            Matcher m = p.matcher(enter); 
             int counter = 0;
-            while (m.find()) { // пока есть к чему ее применить
-                expression[counter] = m.group(1); // получаем информацию между кавычек
+            while (m.find()) { 
+                expression[counter] = m.group(1); 
                 counter += 2;
             }
             String str;
             String str2;
             String[] splitStr;
-            if (expression[2] == null) { // далее в if-else формируем массив из кусочков выражения, решая все проблемы, которые возникают из-за пробелов внутри строки
+            if (expression[2] == null) { 
                 str = enter.replace(expression[0], "");
                 str2 = str.replace("\"", "").substring(1);
                 splitStr = str2.split("\\s+");
@@ -33,7 +33,7 @@ public class Main {
             }
             switch (expression[1]) {
                 case "+":
-                    if (StringManipulator.isCorrectString(expression[0]) && StringManipulator.isCorrectString(expression[2])) { // если ввод корректен, аналогичная проверка везде
+                    if (StringManipulator.isCorrectString(expression[0]) && StringManipulator.isCorrectString(expression[2])) { 
                         StringManipulator.addStrings(expression[0], expression[2]);
                     } else {
                         System.out.println("Enter is incorrect.");
